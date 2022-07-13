ced7816b26b7dfcbd650aeea4bec227943743c8d
Rajesh Nepal <neprazesh@gmail.com>
2022-07-13 22:36:06 +0545

    null & undefined

---
+++ jsConcepts/2. null&undefined.js
@@ -0,0 +1,16 @@
+// What will be logged in the first example and second example?
+
+let var1;
+console.log(var1);
+// logs undefined as nothing is assigned to variable name
+console.log(typeof var1);
+// logs undefined
+// undefined means the variable is declared but the value is not assigned yet inside this variable
+
+
+
+let var2 = null;
+console.log(var2);
+// logs null and null is assigned to var2
+console.log(typeof var2);
+// logs obj as there is no type like null
