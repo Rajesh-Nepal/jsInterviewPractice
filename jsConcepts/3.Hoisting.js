a8758cdaccb505d44c3786657cb53ba34de189a6
Rajesh Nepal <neprazesh@gmail.com>
2022-07-14 22:19:28 +0545

    hoisting

---
+++ jsConcepts/3.Hoisting.js
@@ -0,0 +1,21 @@
+// WHat will be console.logged here
+
+// Hoisting => Means the declaration of your variable will bunnle to the top of the file
+
+// q1.
+console.log(foo);
+foo=1
+// Reference Err:foo is not defined
+
+
+// q2.
+// console.log(foo);
+// var foo=1;
+// consoles the value of foo : undefined
+
+// q2 is similar to 
+// var foo;
+// console.log(foo);
+// foo=2
+
+// var and function bubbles to top of the code
